294f20baf4cff9ea6169f934ad9a04b76835cf63
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-02-12 18:37:50 +0200

    change message to circleci

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello React from Netlify
+         Hello React from Circle CI
         </p>
         <a
           className="App-link"
